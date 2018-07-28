65dc1ac248e73789b104138ad8897b6a719875f1
Teddy Wing <git@teddywing.com>
2018-07-28 08:47:52 +0200

    Get rid of scratch files from initial development
    These are no longer needed.

--- scratchpad.js
+++
@@ -1,7 +0,0 @@
-var x = document.evaluate("//div[contains(@class, 'ytp-settings-menu')]//div[contains(@class, 'ytp-menuitem-label') and contains(., 'Annotations')]/..", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-x.singleNodeValue
-
-var x = document.evaluate("//div[contains(@class, 'ytp-settings-menu')]//div[contains(@class, 'ytp-menuitem-label') and contains(., 'Annotations')]/../div[@aria-checked='true']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-
-var x = document.evaluate("//div[contains(@class, 'ytp-settings-menu')]//div[@aria-checked='true' and //div[contains(@class, 'ytp-menuitem-label') and contains(., 'Annotations')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-var x = document.evaluate("//div[contains(@class, 'ytp-settings-menu')]//div[@aria-checked='true' and //div[contains(@class, 'ytp-menuitem-label') and contains(., 'Annotations')]]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
